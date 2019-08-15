79d5e3dbd354eb105429dcaa4b8867ea89505ba4
Charly <dipta.biswas231998@gmail.com>
2019-08-15 16:58:56 +0530

    This Is Constructor Overloading Example

---
+++ src/Overloading/Calculator.java
@@ -0,0 +1,13 @@
+package Overloading;
+
+public class Calculator {
+
+    public Calculator(int a, int b){
+        System.out.println("The Addition Of Two Numbers "+(a+b));
+    }
+
+    public Calculator(int a, int b, int c){
+        System.out.println("The Addition of Three Integers "+(a+b+c));
+    }
+
+}
---
+++ src/Overloading/Main.java
@@ -0,0 +1,9 @@
+package Overloading;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Calculator(2,3);
+        new Calculator(1,2,3);
+    }
+}
